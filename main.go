513628fd6d21f6eb2f10caf91ac126094ff2d05b
FloDht <flo.11.italia@gmail.com>
2023-12-04 23:39:06 +0100

    Refactor API routes and add endpoints for retrieving categories and grand prix

--- main.go
+++ main.go
@@ -1,28 +1,16 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/flodht/Motorsport-Calendar/entity"
+	"example.com/mod/entity"
+	"github.com/gin-gonic/gin"
 )
 
-// init the main function
 func main() {
-	// init the grand prix
-	grand_prix := entity.GrandPrix{
-		Location: "Monza",
-		Date: "2021-09-12",
-		Name: "Italian Grand Prix",
-	}
+	router := gin.Default()
+	router.GET("/grandprix", entity.GetGrandPrix)
+	router.GET("/grandprix/:id", entity.GetGrandPrixByID)
+	router.GET("/categories", entity.GetCategory)
+	router.GET("/category/:id", entity.GetCategoryByID)
 
-	// init the category
-	category := entity.Category{
-		Name: "Formula 1",
-	}
-
-	// print the grand prix
-	fmt.Println(grand_prix)
-
-	// print the category
-	fmt.Println(category)
+	router.Run("localhost:8080")
 }
